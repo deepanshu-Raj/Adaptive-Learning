0cc4429cca963f0df54ac9b4cc039f8656d1a5c7
deepanshu-Raj <dcode711@gmail.com>
2020-10-24 03:44:17 +0530

    QuizHome & QuizMain Done

--- learning/quizzes/views.py
+++ learning/quizzes/views.py
@@ -6,16 +6,13 @@ from courses.models import *
 def createAssignment(request):
 	if request.method == 'POST':
 		CA = CreateAssignment()
-		
-		# add CA.info = ??
 
-		#CA.info = Course.objects.filter(Course.name==request.POST['info']).first()
-		#print(CA.info)
+		CA.info = Course.objects.filter(name=request.POST['info']).first()
 		CA.created_on = request.POST.get('created_on')
 		CA.title = request.POST['title']
 		CA.desc = request.POST['desc']
-		CA.task  = request.POST['task']
-		#CA.save()
+		CA.task  = request.POST.get('task')
+		CA.save()
 		return render(request,'thanks.html',{
 			'courses':Course.objects.all(),
 			'message':'You have successfully created the assignment!'
@@ -23,4 +20,65 @@ def createAssignment(request):
 	else:
 		return render(request,'createAssignment.html',{
 				'courses':Course.objects.all()
+			})
+
+# Home page for the quiz - complete
+def QuizHome(request):
+	if request.method == 'POST':
+		CQ1 = CreateQuiz_1()
+		
+		CQ1.date = request.POST.get('created_on')
+		CQ1.title = request.POST['title']
+		CQ1.desc = request.POST['desc']
+		CQ1.data = Course.objects.filter(name = request.POST['info']).first()
+		CQ1.save()
+		return render(request,'quizHome.html',{
+			'message':'Your Data has been saved successfully!! Now, You can Now Add problems!!'
+			})
+	else:
+		return render(request,'quizHome.html',{
+			'courses':Course.objects.all()
+			})
+
+#pass data successfully added message here.
+
+#You need to link the user seeting the quiz!!  - !!ISSUE!!
+def QuizMain(request):
+	if request.method == 'POST':
+		CQ2 = CreateQuiz_2()
+		if request.POST.get('save'):
+			
+			# CQ2.data = ??
+			CQ2.question = request.POST['question']
+			CQ2.option1 = request.POST['option1']
+			CQ2.option1 = request.POST['option1']
+			CQ2.option1 = request.POST['option1']
+			CQ2.option1 = request.POST['option1']
+			CQ2.answer = request.POST['answer']
+			CQ2.save()
+			print('save1')
+
+			return render(request,'quizMain.html',{
+
+				})
+
+		elif request.POST.get('exit'):
+			
+			# CQ2.data = ??
+			CQ2.question = request.POST['question']
+			CQ2.option1 = request.POST['option1']
+			CQ2.option1 = request.POST['option1']
+			CQ2.option1 = request.POST['option1']
+			CQ2.option1 = request.POST['option1']
+			CQ2.answer = request.POST['answer']
+			CQ2.save()
+			print('save2')
+
+			return render(request,'thanks.html',{
+				'message':'You have successfully created the quiz!'
+				})
+
+	else:
+		return render(request,'quizMain.html',{
+
 			})
