ea79292747731dfbe300551eff6e6509a80cd90d
deepanshu-Raj <dcode711@gmail.com>
2020-10-30 01:17:06 +0530

    contact Us page done

--- learning/learning/urls.py
+++ learning/learning/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import *
 from . import settings
 from django.conf.urls.static import static
 
@@ -8,7 +8,8 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home, name='home'),
-    
+    path('contact/',Contact,name='contact'),
+    path('aboutus/',aboutUs,name='about'),
     path('', include('accounts.urls', namespace='accounts')),
     # path('chatbot/', include('chatbot.urls', namespace='chatbot')),
     path('courses/', include('courses.urls', namespace='courses')),
--- learning/learning/views.py
+++ learning/learning/views.py
@@ -5,3 +5,17 @@ from courses.models import Course
 def home(request):
     course = Course.objects.filter(author=request.user)
     return render(request, 'home.html', {'course': course})
+
+def aboutUs(request):
+	return render(request,'about.html',{})
+
+def Contact(request):
+	if request.method == 'POST':
+		#work for it's backend !!
+		return render(request,'../accounts/templates/thanks.html',{
+			'user':first_name,
+			'message':'Thank You for your invaluable feedback!!.We will reach to you as soon as possible.',
+			'msg_cnt':'Your Feedback feedback has been sent successfully!!'
+			})
+	else:
+		return render(request,'contact.html',{})
