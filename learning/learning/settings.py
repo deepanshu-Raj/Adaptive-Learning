28ab7938740175c408536973cdd0cdaf782936bf
deepanshu-Raj <dcode711@gmail.com>
2020-10-30 19:48:04 +0530

    package erd

--- learning/learning/settings.py
+++ learning/learning/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'courses',
     'discussions',
     'quizzes',
+    'django_extensions',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
